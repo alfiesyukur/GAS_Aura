251f4f0e1deb634d33d22fed7f060888117c374e
Alfian Syukur <alfi238@gmail.com>
2023-09-28 01:45:37 +0700

    Section 4. Attributes.

--- Source/AuraEditor.Target.cs
+++ Source/AuraEditor.Target.cs
@@ -8,8 +8,8 @@ public class AuraEditorTarget : TargetRules
     public AuraEditorTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V4;
-
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.AddRange(new string[] { "Aura" });
     }
 }
