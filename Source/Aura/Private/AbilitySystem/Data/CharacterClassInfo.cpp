8069164f5d9d82505052b82be4e7c4ea3e6fb5b7
Alfian Syukur <alfi238@gmail.com>
2023-10-13 04:59:01 +0700

    130. Curve Tables - CSV and JSON.

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright sf5gaming.asia
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
